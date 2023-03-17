60c2b8893f26922e095ea17c565cdd03a76a5ee6
Nowerho <Nowerho@gmail.com>
2023-03-17 20:55:17 +0300

    5 коммит

--- front/src/components/header.js
+++ front/src/components/header.js
@@ -1,14 +1,22 @@
 import React from "react";
 
 class Header extends React.Component{
+    var1 = "Текст1"
     render(){
         return(
             <div>
+
+                <input
+                    placeholder={this.var1}
+                    onClick={this.fun1}
+                />
                 <header>dslkfj</header>
-                <h1>vasya</h1>
+                <h1 onClick={this.fun2}>vasya</h1>
             </div>
         )
     }
+    fun1(){console.log("clicked Holder")}
+    fun2(){console.log("clicked Text")}
 }
 
 export default Header
