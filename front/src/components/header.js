2d89607dcf240ca839d9f097a0f0a6e99cfb1d90
Nowerho <Nowerho@gmail.com>
2023-03-17 10:41:14 +0300

    Первый коммит

---
+++ front/src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                <header>dslkfj</header>
+                <h1>vasya</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
